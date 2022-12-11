471cebda069dd80dfe34350980d1262fcf00ec39
Jonatan Fragoso <jonatanfragoso@gmail.com>
2022-12-11 18:34:33 -0500

    subindo att dos arquivos em geral.(incompleto)

--- students/Jonatan/js/index.js
+++ students/Jonatan/js/index.js
@@ -1,12 +1,15 @@
 var ul = document.querySelector('nav ul')
-var menuBtn = document.querySelector('.menu-btn i')
+
 
 function menuShow() {
     if (ul.classList.contains('open')){
         ul.classList.remove('open')
+        document.getElementById("display").style.display = "flex";
+        
 
     }
     else{
         ul.classList.add('open')
+        document.getElementById("display").style.display = "none";
     }
 }
