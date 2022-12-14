fa8996417060f7258005cbae9dcdee56766b171c
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-12-14 14:42:40 -0300

    Merge branch 'main' of https://github.com/Marcos-cmd-henrique/programacao-avancada-frontend-t2-MarcosHenrique

---
+++ students/marcoshenrique/newMobile/scripts.js
@@ -0,0 +1,11 @@
+/* function clickMenu() {
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    }else{
+        itens.style.display = 'block'
+    }
+}
+ */
+function clickMenu(){
+    itens.style.display == 'block'
+}
