f4354c00eac19798cb517cc668e6cf38adbbf8e8
João Victor Mendes <joaodev21@gmail.com>
2022-12-16 20:31:21 -0500

    Adicionando código da aula 16-12-2022

--- students/joao/bootstrap/js/script.js
+++ students/joao/bootstrap/js/script.js
@@ -1,1 +1,63 @@
-console.log('teste'); 
+$(document).ready(function () {
+  let bar = new ProgressBar.Circle(circle__a,{
+    color: '#aaa',
+    strokeWidth: 20,
+    duration: 500,
+    from: { color: '#aaa', width: 1},
+    to: {color: '#aaa', width: 4},
+    step: function (state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      let value = Math.round(circle.value() * 256);
+      circle.setText(value);
+    }
+  });
+
+  bar.animate(1.0);
+
+    bar = new ProgressBar.Circle(circle__b,{
+    color: '#aaa',
+    strokeWidth: 20,
+    duration: 500,
+    from: { color: '#aaa', width: 1},
+    to: {color: '#aaa', width: 4},
+    step: function (state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      let value = Math.round(circle.value() * 50);
+      circle.setText(value);
+    }
+  });
+
+  bar.animate(1.0);
+
+    bar = new ProgressBar.Circle(circle__c,{
+    color: '#aaa',
+    strokeWidth: 20,
+    duration: 500,
+    from: { color: '#aaa', width: 1},
+    to: {color: '#aaa', width: 4},
+    step: function (state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      let value = Math.round(circle.value() * 1500);
+      circle.setText(value);
+    }
+  });
+  bar.animate(1.0);
+
+    bar = new ProgressBar.Circle(circle__d,{
+    color: '#aaa',
+    strokeWidth: 20,
+    duration: 500,
+    from: { color: '#aaa', width: 1},
+    to: {color: '#aaa', width: 4},
+    step: function (state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      let value = Math.round(circle.value() * 500);
+      circle.setText(value);
+    }
+  });
+  bar.animate(1.0);
+
+});
+
+$('#numbers-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+
