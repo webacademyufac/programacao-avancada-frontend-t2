bc87045d825cd272fdede5b1cfc6ae2bc53a8f5f
Ezequiel da Slva Oliveira <95730033+EzequielSOliveira@users.noreply.github.com>
2022-12-16 20:13:38 -0500

    Atualização da aula 16/12/2022

--- students/ezequiel/bootstrap/js/scripts.js
+++ students/ezequiel/bootstrap/js/scripts.js
@@ -1,1 +1,32 @@
-console.log('Working...')
+console.log('Working...')   
+
+/* Progressbar */
+function circle(id, text) {
+
+    let bar = new ProgressBar.Circle(id, {
+        color: '#eee',
+        strokeWidth: 15,
+        duration: 500,
+        from: { color: '#eee', width: 1 },
+        to: { color: '#ddd', width: 4 },
+        step: function (state, circle) {
+            circle.path.setAttribute('stroke', state.color);
+            var value = Math.round(circle.value() * text);
+            circle.setText(value);
+
+        }
+    });
+    bar.animate(1.0);  // Number from 0.0 to 1.0
+}
+
+$(document).ready(function () {
+    /* Progressbar */
+    circle('#circleA', 100)
+    circle('#circleB', 200)
+    circle('#circleC', 300)
+    circle('#circleD', 400)
+
+    /* parallax */
+    $('#numbers-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+    $('#pattern-img').parallax({imageSrc: 'img/pattern.png'});
+});
