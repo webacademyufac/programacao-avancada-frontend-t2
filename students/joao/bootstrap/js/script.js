270c79d5aa8b613ddf6d178fe6bc0b4f8438f778
João Victor Mendes <joaodev21@gmail.com>
2022-12-19 17:36:36 -0500

    Atualização da última aula do dia 16-12-2022

--- students/joao/bootstrap/js/script.js
+++ students/joao/bootstrap/js/script.js
@@ -58,6 +58,84 @@ $(document).ready(function () {
   bar.animate(1.0);
 
 });
+/*
+     Parallax 
+    
+    $('#numbers-area').parallax({ imageSrc: 'img/cidadeparallax.png' });
+    $('#pattern-img').parallax({ imageSrc: 'img/pattern.png' });
 
-$('#numbers-area').parallax({imageSrc: 'img/cidadeparallax.png'});
+*/
+
+/* Filtro da seção de portfólio */
+    
+$('.filter-btn').on('click', function () {
+
+  let type = $(this).attr('id');
+  let boxes = $('.project-box');
+
+  $('.main-btn').removeClass('active');
+  $(this).addClass('active');
+
+  if (type == 'dsg-btn') {
+      eachBoxes('dsg', boxes);
+  } else if (type == 'dev-btn') {
+      eachBoxes('dev', boxes);
+  } else if (type == 'seo-btn') {
+      eachBoxes('seo', boxes);
+  } else {
+      eachBoxes('all', boxes);
+  }
+
+});
+
+function eachBoxes(type, boxes) {
+
+  if (type == 'all') {
+      $(boxes).fadeIn();
+  } else {
+      $(boxes).each(function () {
+          if (!$(this).hasClass(type)) {
+              $(this).fadeOut('slow');
+          } else {
+              $(this).fadeIn();
+          }
+      });
+  }
+}
+
+// Scroll do menu para as seções
+
+let navBtn = $('.nav-item');
+
+let bannerSection = $('#mainSlider');
+let aboutSection = $('#about-area');
+let servicesSection = $('#services-area');
+let teamSection = $('#team-area');
+let portfolioSection = $('#portfolio-area');
+let contactSection = $('#contact-area');
+
+let scrollTo = '';
+
+$(navBtn).click(function () {
+
+  let btnId = $(this).attr('id');
+
+  if (btnId == 'about-menu') {
+      scrollTo = aboutSection;
+  } else if (btnId == 'services-menu') {
+      scrollTo = servicesSection;
+  } else if (btnId == 'team-menu') {
+      scrollTo = teamSection;
+  } else if (btnId == 'portfolio-menu') {
+      scrollTo = portfolioSection;
+  } else if (btnId == 'contact-menu') {
+      scrollTo = contactSection;
+  } else {
+      scrollTo = bannerSection;
+  }
+
+  $([document.documentElement, document.body]).animate({
+      scrollTop: $(scrollTo).offset().top - 70
+  }, 1500);
+});
 
